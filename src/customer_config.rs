dc767900dbaf09e3a5510be4586a0e192bec24fe
mtu <michal.tumilowicz01@gmail.com>
2023-12-23 22:42:25 +0100

    naming

--- src/customer_config.rs
+++ src/customer_config.rs
@@ -3,13 +3,13 @@ use std::sync::RwLock;
 use crate::customer::{Customer, CustomerId, CustomerRepository, CustomerError};
 
 pub struct CustomerInMemoryRepository {
-    requests: RwLock<HashMap<CustomerId, Customer>>,
+    customers: RwLock<HashMap<CustomerId, Customer>>,
 }
 
 impl CustomerInMemoryRepository {
     pub fn new() -> Self {
         CustomerInMemoryRepository {
-            requests: RwLock::new(HashMap::new()),
+            customers: RwLock::new(HashMap::new()),
         }
     }
 }
@@ -19,7 +19,7 @@ impl CustomerRepository for CustomerInMemoryRepository {
     fn create(&self, customer: Customer) -> Result<Customer, CustomerError> {
         match self.get_by_id(customer.id()) {
             None => {
-                self.requests.write().unwrap().insert(customer.id().clone(), customer.clone());
+                self.customers.write().unwrap().insert(customer.id().clone(), customer.clone());
                 Ok(customer.clone())
             }
             Some(_) =>
@@ -30,6 +30,6 @@ impl CustomerRepository for CustomerInMemoryRepository {
     }
 
     fn get_by_id(&self, customer_id: &CustomerId) -> Option<Customer> {
-        self.requests.read().unwrap().get(customer_id).cloned()
+        self.customers.read().unwrap().get(customer_id).cloned()
     }
 }
