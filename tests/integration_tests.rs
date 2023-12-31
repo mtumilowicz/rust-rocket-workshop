71f2824c387c15804ae18972a5b8f54b1f37dee9
mtu <michal.tumilowicz01@gmail.com>
2024-01-01 00:31:55 +0100

    simplifying tests

--- tests/integration_tests.rs
+++ tests/integration_tests.rs
@@ -1,42 +1,53 @@
 use rocket::async_test;
 use rocket::http::{ContentType, Status};
 use rocket::local::asynchronous::Client;
-use rocket::serde::json::serde_json;
-use rust_rocket_workshop::gateway::customer::*;
+use rocket::serde::json::{serde_json, Value};
+use rocket::serde::json::serde_json::json;
 use rust_rocket_workshop::app;
 
 #[async_test]
 async fn test_create_and_get_customer() {
+    // given
     let rocket = app::server().await;
     let client = Client::tracked(rocket).await.expect("valid rocket instance");
 
+    // and
     let new_customer_input = r#"
         {
             "name": "John Doe"
         }
     "#;
 
+    // when
     let create_response = client.post("/api/customers")
         .header(ContentType::JSON)
         .body(new_customer_input)
         .dispatch()
         .await;
 
+    // then
     assert_eq!(create_response.status(), Status::Created);
 
+    // when
     let customer_id = create_response.headers().get_one("Location").unwrap();
-
     let get_response = client.get(format!("/api/customers/{}", customer_id))
         .dispatch()
         .await;
 
+    // then
     assert_eq!(get_response.status(), Status::Ok);
 
-    let customer_output: CustomerApiOutput = serde_json::from_str(&get_response.into_string().await.unwrap()).unwrap();
+    // and
+    let result: Value = serde_json::from_str(&get_response.into_string().await.unwrap()).unwrap();
+    let expected_result: Value = json!(
+        {
+            "id": customer_id,
+            "name": "John Doe",
+            "locked": false
+        }
+    );
 
-    assert_eq!(customer_output.id(), customer_id);
-    assert_eq!(customer_output.name(), "John Doe");
-    assert_eq!(customer_output.locked(), false);
+    assert_eq!(&result, &expected_result);
 }
 
 #[async_test]
